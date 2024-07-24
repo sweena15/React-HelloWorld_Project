a5dfb55f72e238fd64ceba764b20fba656445974
Sharvin <gavad.sharvin@gmail.com>
2024-07-24 10:42:56 +0530

    Initial commit

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+// src/components/Home.js
+import React from 'react';
+// import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>Welcome to the Home Page</h1>
+        <p>
+          This is your default home page component.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
